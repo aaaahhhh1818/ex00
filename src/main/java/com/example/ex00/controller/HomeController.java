503cd6bfa7ad2838d7c2007768838045fffcf7bd
ahyun <aaaaahhhhh1818@hanmail.net>
2021-08-27 19:11:08 +0900

    20210827

---
+++ src/main/java/com/example/ex00/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.example.ex00.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import com.example.ex00.dto.SampleDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Log4j2
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        log.info("Welcome home! The client locale is {}." + locale);
+        log.info("Welcome home! The client locale is {}." + locale);
+        log.info("Welcome home! The client locale is {}." + locale);
+        log.info("Welcome home! The client locale is {}." + locale);
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        //객체만들기
+        SampleDTO dto = new SampleDTO();
+        dto.setFirst("Hong");
+        dto.setLast("Gil Dong");
+
+        model.addAttribute("serverTime", formattedDate );
+        model.addAttribute("dto", dto );
+
+        return "home";
+    }
+
+}
+
